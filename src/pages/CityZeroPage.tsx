46ce932cb565d16af3d8d567689bd7dc6c64d57e
Pace Philpot <2504970020@qq.com>
2026-02-22 14:09:50 +0800

    update

--- src/pages/CityZeroPage.tsx
+++ src/pages/CityZeroPage.tsx
@@ -10,22 +10,26 @@ const CityZeroPage: React.FC = () => {
             <section className="hero-section">
                 <div className="scrolling-announcements">
                     <div className="scrolling-text">
-                        <span>&lt;&lt;&lt;&lt; Scrolling Announcements</span>
-                        <span>&lt;&lt;&lt; Scrolling Announcements</span>
-                        <span>Scrolling Announcements</span>
-                        <span>&lt;&lt;&lt;Scrolling Announcements</span>
-                        <span>&lt;&lt;&lt;&lt; Scrolling Announcements</span>
-                        <span>&lt;&lt;&lt; Scrolling Announcements</span>
-                        <span>Scrolling Announcements</span>
-                        <span>&lt;&lt;&lt;Scrolling Announcements</span>
+                        <span className="text-color-1">&lt;&lt;&lt;&lt; SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-2">&lt;&lt;&lt; SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-3">SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-4">&lt;&lt;&lt; SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-1">&lt;&lt;&lt;&lt; SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-2">&lt;&lt;&lt; SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-3">SCROLLING ANNOUNCEMENTS</span>
+                        <span className="text-color-4">&lt;&lt;&lt; SCROLLING ANNOUNCEMENTS</span>
                     </div>
                 </div>
                 <div className="hero-image-container">
                     <img src="/CityZero/city.png" alt="City Zero" className="hero-image" />
-                    {/* Stadium Hotspot Link */}
-                    <Link to="/city-zero/stadium" className="stadium-hotspot-btn" title="Enter Stadium">
-                        <div className="hotspot-pulse"></div>
-                        <span className="hotspot-text">Enter Stadium</span>
+                    {/* Stadium Action Button */}
+                    <Link to="/city-zero/stadium" className="stadium-action-btn" title="Enter Stadium">
+                        <span className="btn-text">ENTER STADIUM</span>
+                        <div className="btn-icon">
+                            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5 12H19M19 12L12 5M19 12L12 19" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </div>
                     </Link>
                 </div>
             </section>
